711e5e330965bc61b06219740babc10709a9cff2
ehsan <e@ewerx.ca>
2019-04-07 14:42:29 -0400

    migrate to Swift 5

--- BWSwipeRevealCellExample/MasterViewController.swift
+++ BWSwipeRevealCellExample/MasterViewController.swift
@@ -150,6 +150,8 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         case .move:
             tableView.deleteRows(at: [indexPath!], with: .fade)
             tableView.insertRows(at: [newIndexPath!], with: .fade)
+        @unknown default:
+            assertionFailure()
         }
     }
     
